1c631fdaeb1ca7540ac4ddf7a2c3f77ccd07f5bd
Firas El Rachidi <36714331+firasrashidi@users.noreply.github.com>
2022-06-07 18:16:43 -0400

    Add confusion_matrix_generation

--- app/eval.py
+++ app/eval.py
@@ -1,17 +1,8 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sn
 import torch
-from sklearn.metrics import (
-    classification_report,
-    confusion_matrix,
-    f1_score,
-    precision_score,
-)
+from sklearn.metrics import classification_report
 
-from dataset import dataset, single_test_dl, test_dl, train_dl
-from util import test_stats
+from dataset import dataset, single_test_dl, test_dl
+from util import construct_confusion_matrix, test_stats
 
 cnn_v1 = torch.load("./model/CNNV1.pb")
 cnn_v2 = torch.load("./model/CNNV2.pb")
@@ -25,6 +16,7 @@ print(
 print(
     f"Classification Report for v1: \n {classification_report(labels_v1, predicted_v1, zero_division=1)}"
 )
+construct_confusion_matrix(labels_v1, predicted_v1, "conf_matrix_v1")
 
 
 predicted_v2, labels_v2, correct_v2, total_v2 = test_stats(cnn_v2, test_dl)
@@ -34,6 +26,7 @@ print(
 print(
     f"Classification Report for v2: \n {classification_report(labels_v2, predicted_v2, zero_division=1)}"
 )
+construct_confusion_matrix(labels_v2, predicted_v2, "conf_matrix_v2")
 
 predicted_v3, labels_v3, correct_v3, total_v3 = test_stats(cnn_v3, test_dl)
 print(
@@ -42,17 +35,7 @@ print(
 print(
     f"Classification Report for v3: \n {classification_report(labels_v3, predicted_v3, zero_division=1)}"
 )
+construct_confusion_matrix(labels_v3, predicted_v3, "conf_matrix_v3")
 
 predicted_single, _, _, _ = test_stats(cnn_v3, single_test_dl)
 print(f"The current model predicted: {dataset.classes[predicted_single[0]]}")
-
-# cf_matrix = confusion_matrix(labels, predicted)
-# df_cm = pd.DataFrame(
-#     cf_matrix / np.sum(cf_matrix) * 10,
-#     index=[i for i in dataset.classes],
-#     columns=[i for i in dataset.classes],
-# )
-# plt.figure(figsize=(12, 7))
-# sn.heatmap(df_cm, annot=True)
-# plt.savefig("output1.png")
-# print(classification_report(labels, predicted))
--- app/util.py
+++ app/util.py
@@ -1,4 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sn
 import torch
+from sklearn.metrics import confusion_matrix
+
+from dataset import dataset
 
 
 def test_stats(model, data_loader, device="cpu"):
@@ -12,3 +19,15 @@ def test_stats(model, data_loader, device="cpu"):
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
     return predicted, labels, correct, total
+
+
+def construct_confusion_matrix(labels, predicted, filename, dataset=dataset):
+    cf_matrix = confusion_matrix(labels, predicted)
+    df_cm = pd.DataFrame(
+        cf_matrix / np.sum(cf_matrix) * 10,
+        index=[i for i in dataset.classes],
+        columns=[i for i in dataset.classes],
+    )
+    plt.figure(figsize=(12, 7))
+    sn.heatmap(df_cm, annot=True)
+    plt.savefig(f"{filename}.png")
