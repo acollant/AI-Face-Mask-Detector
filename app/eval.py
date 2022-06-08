fb27f0e019e1a01b66f11e7e429bbd48e1229b96
Firas El Rachidi <36714331+firasrashidi@users.noreply.github.com>
2022-06-08 01:18:06 -0400

    Fix confusion matrix issue

--- app/eval.py
+++ app/eval.py
@@ -14,17 +14,16 @@ print(
     f"Accuracy of the model on the {total_v1} (v1) test images: {round(100 * correct_v1 / total_v1, 2)} %"
 )
 print(
-    f"Classification Report for v1: \n {classification_report(labels_v1, predicted_v1, zero_division=1)}"
+    f"Classification Report for v1: \n {classification_report(labels_v1, predicted_v1, zero_division=1, target_names=dataset.classes)}"
 )
 construct_confusion_matrix(labels_v1, predicted_v1, "conf_matrix_v1")
-
 
 predicted_v2, labels_v2, correct_v2, total_v2 = test_stats(cnn_v2, test_dl)
 print(
     f"Accuracy of the model on the {total_v2} (v2) test images: {round(100 * correct_v2 / total_v2, 2)} %"
 )
 print(
-    f"Classification Report for v2: \n {classification_report(labels_v2, predicted_v2, zero_division=1)}"
+    f"Classification Report for v2: \n {classification_report(labels_v2, predicted_v2, zero_division=1, target_names=dataset.classes)}"
 )
 construct_confusion_matrix(labels_v2, predicted_v2, "conf_matrix_v2")
 
@@ -33,7 +32,7 @@ print(
     f"Accuracy of the model on the {total_v3} test images: {round(100 * correct_v3 / total_v3, 2)} %"
 )
 print(
-    f"Classification Report for v3: \n {classification_report(labels_v3, predicted_v3, zero_division=1)}"
+    f"Classification Report for v3: \n {classification_report(labels_v3, predicted_v3, zero_division=1, target_names=dataset.classes)}"
 )
 construct_confusion_matrix(labels_v3, predicted_v3, "conf_matrix_v3")
 
